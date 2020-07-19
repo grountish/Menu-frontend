9e86374175e43bc98da3e6c4fd152d79dd956f4d
MilttO0nMss <grountish@gmail.com>
2020-07-19 21:15:13 +0200

    before sofia merge

--- src/Components/FoodCategory.js
+++ src/Components/FoodCategory.js
@@ -8,7 +8,7 @@ export default class FoodCategory extends Component {
       
         return (
             <div className="box" style={{height:`calc(75vh/${foods.categorias.length})`, alignItems:"center", display:"flex"}}>
-                    <h1 style={{fontSize:`calc(65vh/${foods.categorias.length}/2.5)`, margin:"auto" }}>{this.props.lang === "en" ? this.props.nombre_en : this.props.nombre}</h1>
+                    <h1 className="headerCategory" style={{fontSize:`calc(65vh/${foods.categorias.length}/2.5)`, margin:"auto" }}>{this.props.lang === "en" ? this.props.nombre_en : this.props.nombre}</h1>
           </div>
         )
     }
