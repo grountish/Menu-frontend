1e5e4651e0def562e6140fe4bb007c2ebaf19fcb
MilttO0nMss <grountish@gmail.com>
2020-07-19 20:19:27 +0200

    b4 merge chango

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 //import 'bulma/css/bulma.css';
-import CategoryList from './Components/CategoryList';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Foodlist from './Components/Foodlist';
-import SearchComponent from './Components/SearchComponent';
-import foods from './foods.json';
-import Helmet from 'react-helmet';
+import CategoryList from "./Components/CategoryList";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Foodlist from "./Components/Foodlist";
+import SearchComponent from "./Components/SearchComponent";
+import foods from "./foods.json";
+import Helmet from "react-helmet";
 
 function App() {
   const [buscar, setBuscar] = useState(false);
+  const [lang,setLang]=useState("es")
+
 
   return (
     <div className="App">
@@ -18,20 +20,27 @@ function App() {
           <style>{`body { background-color: ${foods.backgroundColor}; min-height:100vh; font-family: ${foods.font}; color: ${foods.color}}`}</style>
         </Helmet>
         <Link to="/">
-          {' '}
+          {" "}
           <img src={foods.iso} alt="logo" className="isoTipo" />
         </Link>
         <input
           type="button"
           onClick={() => setBuscar(!buscar)}
-          value={buscar ? 'volver' : 'buscar'}
+          value={buscar ? "volver" : "buscar"}
         />
+        
+         
         {buscar ? (
           <SearchComponent />
         ) : (
           <div>
-            <Route path="/" exact component={CategoryList} />
-            <Route path="/category/:categoryName" exact component={Foodlist} />
+            <div>
+              <button onClick={()=>setLang("en")}>EN</button>
+              <button onClick={()=>setLang("es")}>ES</button>
+            </div>
+
+            <Route path="/" exact render={()=><CategoryList lang={lang}/>} />
+            <Route path="/category/:categoryName" exact component={Foodlist}  lang={lang}/>
           </div>
         )}
       </Router>
--- src/Components/FoodCategory.js
+++ src/Components/FoodCategory.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 export default class FoodCategory extends Component {
-    render() {
+
+ 
+  render() {
+      
         return (
             <div className="box">
-                    <h1>{this.props.nombre}</h1>
+                    <h1>{this.props.lang === "en" ? this.props.nombre_en : this.props.nombre}</h1>
           </div>
         )
     }
