31ae980a8a60696f6ae08f26526c0a59da01b93b
changodiez <joaquindiezw@gmail.com>
2020-07-24 01:33:38 +0200

    mucho ruido pocas nueces

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useIntersection } from "react-use"
+
 
 import Carousel from "nuka-carousel";
 import "./../../src/Home.css";
@@ -10,15 +12,50 @@ import {
   faAngleDoubleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
+
 import movil from "./../Assets/movil.png";
+import gsap from "gsap";
+import { Power1 } from "gsap/gsap-core";
+
 
 const Home = () => {
  
-  const [navClass, setNavClass] = useState(true);
-window.addEventListener('scroll', () => {
-  const isScroll = window.scrollY > 200 
-  console.log(window.scrollY)
+const [navClass, setNavClass] = useState(true);
+const [colorClass, setColorClass] = useState(false);
+
+const sectionRef = useRef (null)
+const intersection = useIntersection(sectionRef, {
+  root:null,
+  rootMargin: "0px",
+  threshold: 0.2
 })
+
+const fadeIn = element => {
+  gsap.to(element,1, {
+    opacity: 1,
+    marker: true,
+    x: 0,
+    ease: "power4.out",
+    stagger: {
+      amount: 2
+    }
+  })
+}
+
+const fadeOut = element => {
+  gsap.to(element,1, {
+    opacity: 0,
+    x: "-100vw",
+    ease: "power4.out",
+    stagger: {
+      amount: 1.8
+    }
+  })
+}
+
+intersection && intersection.intersectionRatio < 0.2 ? fadeOut(".fadeIn") : fadeIn(".fadeIn")
+
+
   return (
     <div className="home">
       <div
@@ -60,31 +97,35 @@ window.addEventListener('scroll', () => {
       >
         <section className="Inicio" id="Inicio"></section>
 
-        <section id="Nosotros" className="Nosotros">
+        <section ref={sectionRef} id="Nosotros" className="Nosotros">
           <div className="frase">
             <h5>
-            <span className="palabra-clave-verde">Collabo studio</span> surge como unión de un colectivo diverso dedicado
-              al sector digital. De las áreas de <span className="palabra-clave-verde">dirección de arte</span>, estrategia,
-              desarrollo web, creative coding, <span className="palabra-clave-verde">diseño gráfico</span>, fotografía y
+            <span onMouseOver={() => setColorClass(!colorClass)}  onMouseOut={() => setColorClass(!colorClass)} className={ colorClass ? "palabra-clave-verde" : null}>Collabo studio</span> surge como unión de un colectivo diverso dedicado
+              al sector digital. De las áreas de <span onMouseOver={() => setColorClass(!colorClass)}  onMouseOut={() => setColorClass(!colorClass)} className={ colorClass ? "palabra-clave-verde" : null}>dirección de arte</span>, estrategia,
+              desarrollo web, creative coding, <span onMouseOver={() => setColorClass(!colorClass)}  onMouseOut={() => setColorClass(!colorClass)} className={ colorClass ? "palabra-clave-verde" : null}>diseño gráfico</span>, fotografía y
               community management, nos unimos para dar vida a marcas dinámicas
-              con alto <span className="palabra-clave-verde">impacto digital</span>.
+              con alto <span onMouseOver={() => setColorClass(!colorClass)}  onMouseOut={() => setColorClass(!colorClass)} className={ colorClass ? "palabra-clave-verde" : null}>impacto digital</span>.
             </h5>
           </div>
         </section>
 
-        <section id="Servicios" className="Servicios"></section>
+        <section id="Servicios" className="Servicios">
+          <ul  className="servicios-list fadeIn">
+            <li className="fadeIn">Diseño Grafico</li>
+            <li  className="fadeIn">Fotografía</li>
+            <li  className="fadeIn">Desarrollo Web</li>
+            <li className="fadeIn">Branding</li>
+            <li className="fadeIn">Creative Coding</li>
+          </ul>
+        </section>
 
         <section id="MenuQR" className="MenuQR">
           <Carousel>
             <div className="menuQR-1">
               <div className="texto">
                 <h2>
-                  Las cartas seguras llegaron para darle batalla al covid-19
+                  Las cartas QR llegaron para protegernos de la amenaza Covid-19. Conoce mas sobre la nueva opción que ademas cuida el planeta.
                 </h2>
-                <p>
-                  Conoce todas las ventajas sobre esta nueva opción que cuida a
-                  las personas y el planeta ahre
-                </p>
                 <div>
                   <h3>Desliza para saber mas!</h3>
                   <h3>
