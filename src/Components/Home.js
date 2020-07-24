f6d8257c36237573c7fdb4deda0a2ee449737807
Sofia Santa Maria <yolaresofia@gmail.com>
2020-07-24 12:33:42 +0200

    p5 instance mode trying to unmount component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
 import React,{ useState } from "react";
-
+import Sketch from './Sketch.jsx'
 import "./../../src/Home.css";
 
 const Home = () => {
@@ -33,12 +33,12 @@ const Home = () => {
         </div>
       </nav>
          <div className="toggle-btn" onClick={()=>setNavClass(!navClass)}>
-        <span>{navClass ? "III" : "X"}</span>
+        <span style={{fontFamily: 'OPTIImprovNewWideNine', fontSize:'0.7em'}}></span>
          </div>
       <main>
         <section className="Inicio" id="Inicio">
-            <div id="sketch"></div>
-    <img src="./../Assets/ruido.jpg" id="ruido" alt=""/>
+        <Sketch />
+        <img src="./../Assets/ruido.jpg" id="ruido" alt=""/>
         </section>
         <section id="Nosotros" className="Nosotros">
           <p class="nosotres-texto">
@@ -52,7 +52,9 @@ De las áreas de dirección de arte, estrategia, desarrollo web, creative coding
         </section>
 
         <section id="Contacto" className="Contacto">
-
+        <div>
+          
+        </div>
         </section>
       </main>
     </div>
